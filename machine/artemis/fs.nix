39de3588303cc3c92ef9aaa2d34bb8da7602b4bf
Ash Walker <ashurstwalker@gmail.com>
2024-04-17 11:18:32 -0400

    feat(artemis): nfs

---
+++ machine/artemis/fs.nix
@@ -0,0 +1,37 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+with builtins; let
+  std = pkgs.lib;
+in {
+  options = with lib; {};
+  disabledModules = [];
+  imports = [];
+  config = {
+    services.rpcbind = {
+      enable = true;
+    };
+    systemd.mounts = [
+      {
+        type = "nfs";
+        mountConfig = {
+          Options = "noatime";
+        };
+        what = "server:/172.24.86.1";
+        where = "/elysium";
+      }
+    ];
+    systemd.automounts = [
+      {
+        automountConfig = {
+          TimeoutIdleSec = "720";
+        };
+        where = "/elysium";
+      }
+    ];
+  };
+  meta = {};
+}
