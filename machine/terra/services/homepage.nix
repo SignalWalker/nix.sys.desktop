77955bc563f539e7872d5284aa569dffc1d0acfd
Ash Walker <ashurstwalker@gmail.com>
2024-06-15 10:34:31 -0400

    ...

---
+++ machine/terra/services/homepage.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+with builtins; let
+  std = pkgs.lib;
+  homepage = config.services.homepage-dashboard;
+  domain = "home.terra.ashwalker.net";
+in {
+  options = with lib; {
+  };
+  disabledModules = [];
+  imports = [];
+  config = {
+    services.homepage-dashboard = {
+      enable = false;
+      listenPort = 30801;
+      openFirewall = lib.mkForce false;
+      startUrl = "http://${domain}";
+    };
+  };
+  meta = {};
+}
