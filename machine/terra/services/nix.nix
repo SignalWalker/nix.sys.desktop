d634e83d25603f34a6dbeb1c7df511722e6a72dd
Ash Walker <ashurstwalker@gmail.com>
2024-09-11 12:24:55 -0400

    ...

--- machine/terra/services/nix.nix
+++ machine/terra/services/nix.nix
@@ -7,19 +7,47 @@
 with builtins; let
   std = pkgs.lib;
   secrets = config.age.secrets;
+  serve = config.services.nix-serve;
 in {
-  options = with lib; {};
+  options = with lib; {
+    services.nix-serve = {
+      user = mkOption {
+        type = types.str;
+        readOnly = true;
+        default = "nix-serve";
+      };
+      group = mkOption {
+        type = types.str;
+        readOnly = true;
+        default = "nix-serve";
+      };
+    };
+  };
   disabledModules = [];
   imports = [];
   config = {
     age.secrets.nixStoreKey = {
       file = ./nix/nixStoreKey.age;
+      owner = serve.user;
+      group = serve.group;
     };
     nix = {
-      sshServe = {
-        write = true;
-      };
       settings.secret-key-files = [secrets.nixStoreKey.path];
+    };
+    services.nix-serve = {
+      enable = true;
+      secretKeyFile = secrets.nixStoreKey.path;
+      port = 42533;
+    };
+    users.users.${serve.user} = {
+      isSystemUser = true;
+      inherit (serve) group;
+    };
+    users.groups.${serve.group} = {};
+    services.nginx.virtualHosts."nix-cache.terra.ashwalker.net" = {
+      locations."/" = {
+        proxyPass = "http://${serve.bindAddress}:${toString serve.port}";
+      };
     };
   };
   meta = {};
--- machine/terra/services/postgresql.nix
+++ machine/terra/services/postgresql.nix
@@ -6,16 +6,59 @@
 }:
 with builtins; let
   std = pkgs.lib;
+  pg = config.services.postgresql;
+  pgbck = config.services.postgresqlBackup;
+  shouldUpgrade = pg.package.psqlSchema != pkgs.postgresql.psqlSchema;
 in {
   options = with lib; {};
   disabledModules = [];
   imports = [];
   config = {
+    warnings = lib.mkIf shouldUpgrade [
+      "postgresql upgrade available (${pg.package.psqlSchema} -> ${pkgs.postgresql.psqlSchema}); use `sudo upgrade-pg-cluster` to upgrade"
+    ];
     services.postgresql = {
       enable = true;
-      package = pkgs.postgresql;
-      extraPlugins = with pkgs.postgresql.pkgs; [];
+      package = pkgs.postgresql_16;
+      extraPlugins = ps: with ps; [];
     };
+    services.postgresqlBackup = {
+      enable = pg.enable;
+      location = "/elysium/backup/postgres";
+      compression = "zstd";
+      compressionLevel =
+        if pgbck.compression == "zstd"
+        then 19
+        else 9;
+    };
+    environment.systemPackages = lib.mkIf shouldUpgrade [
+      (let
+        newPg = pkgs.postgresql.withPackages pg.extraPlugins;
+      in
+        pkgs.writeScriptBin "upgrade-pg-cluster" ''
+          set -eux
+          # XXX it's perhaps advisable to stop all services that depend on postgresql
+          systemctl stop postgresql
+
+          export NEWDATA="/var/lib/postgresql/${newPg.psqlSchema}"
+
+          export NEWBIN="${newPg}/bin"
+
+          export OLDDATA="${pg.dataDir}"
+          export OLDBIN="${pg.package}/bin"
+
+          install -d -m 0700 -o postgres -g postgres "$NEWDATA"
+          cd "$NEWDATA"
+          sudo -u postgres $NEWBIN/initdb -D "$NEWDATA"
+
+          sudo -u postgres $NEWBIN/pg_upgrade \
+            --old-datadir "$OLDDATA" --new-datadir "$NEWDATA" \
+            --old-bindir $OLDBIN --new-bindir $NEWBIN \
+            --jobs $(nproc) \
+            --link \
+            "$@"
+        '')
+    ];
   };
   meta = {};
 }
