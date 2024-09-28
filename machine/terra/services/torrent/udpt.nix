5890bbf4d6663054a0f38d9731aa8c8111d82145
Ash Walker <ashurstwalker@gmail.com>
2024-09-28 19:12:12 -0400

    feat: udpt service

---
+++ machine/terra/services/torrent/udpt.nix
@@ -0,0 +1,66 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+with builtins; let
+  std = pkgs.lib;
+  udpt = config.service.udpt;
+  toml = pkgs.formats.toml {};
+in {
+  options = with lib; {
+    services.udpt = {
+      enable = mkEnableOption "udpt torrent tracker";
+      package = mkPackageOption pkgs "updt" {};
+      user = mkOption {
+        type = types.str;
+        readonly = true;
+        default = "udpt";
+      };
+      group = mkOption {
+        type = types.str;
+        readonly = true;
+        default = "udpt";
+      };
+      port = mkOption {
+        type = types.port;
+        default = 42784;
+      };
+      settings = mkOption {
+        type = toml.type;
+        default = {};
+      };
+      settingsFile = mkOption {
+        type = types.path;
+        readOnly = true;
+        default = toml.generate "udpt.toml" udpt.settings;
+      };
+    };
+  };
+  disabledModules = [];
+  imports = [];
+  config = lib.mkIf udpt.enable {
+    users.users.${udpt.user} = {
+      isSsytemUser = true;
+      inherit (udpt) group;
+    };
+    users.groups.${udpt.group} = {};
+
+    networking.firewall.allowedUDPPorts = [udpt.port];
+
+    systemd.services."udpt" = {
+      after = ["network-online.target" "nss-lookup.target"];
+      wants = ["network-online.target"];
+      # wantedBy = ["multi-user.target"];
+      description = "udpt torrent tracker";
+      path = [udpt.package];
+      serviceConfig = {
+        ExecStart = "${udpt.package}/bin/udpt -c ${udpt.configFile}";
+        User = udpt.user;
+        Group = udpt.group;
+      };
+    };
+  };
+  meta = {};
+}
