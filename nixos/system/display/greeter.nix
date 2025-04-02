657700faac5cb668b832527913875f7475929f51
Ash Walker <ashurstwalker@gmail.com>
2025-04-02 18:08:40 -0400

    it's not the display manager?

--- nixos/system/display/greeter.nix
+++ nixos/system/display/greeter.nix
@@ -12,9 +12,29 @@ in {
   disabledModules = [];
   imports = [];
   config = {
-    programs.regreet = {
-      enable = false;
-    };
+    # programs.regreet = {
+    #   enable = false;
+    # };
+    #
+    # services.displayManager = {
+    #   sddm = {
+    #     enable = false;
+    #     wayland.enable = false;
+    #   };
+    #   ly = {
+    #     enable = false;
+    #     settings = {
+    #       animation = "colormix";
+    #       bigclock = "en";
+    #       brightness_up_cmd = "light -A 2";
+    #       brightness_down_cmd = "light -U 2";
+    #       brightness_up_key = "XF86MonBrightnessUp";
+    #       brightness_down_key = "XF86MonBrightnessDown";
+    #       clear_password = true;
+    #       clock = "%Y-%m-%d %H:%M";
+    #     };
+    #   };
+    # };
 
     services.greetd = let
       greetd = config.services.greetd;
