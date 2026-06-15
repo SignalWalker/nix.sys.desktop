31071bbf2a90d1995d378fca6aaa83248c9e8dfd
Ash Walker <ashurstwalker@gmail.com>
2026-06-15 10:18:14 -0700

    kmscon: fix deprecated options

--- nixos/system/display.nix
+++ nixos/system/display.nix
@@ -4,24 +4,25 @@
   lib,
   ...
 }:
-with builtins;
 let
-  std = pkgs.lib;
-  xserver = config.services.xserver;
-  manager = config.services.desktopManager.manager;
+  inherit (builtins) foldl' attrNames;
 in
 {
-  options = with lib; {
-    services.desktopManager.manager = mkOption {
-      type = types.enum [
-        "plasma6"
-        "sway"
-        "river"
-        "hyprland"
-      ];
-      default = "hyprland";
+  options =
+    let
+      inherit (lib) mkOption types;
+    in
+    {
+      services.desktopManager.manager = mkOption {
+        type = types.enum [
+          "plasma6"
+          "sway"
+          "river"
+          "hyprland"
+        ];
+        default = "hyprland";
+      };
     };
-  };
   disabledModules = [ ];
   imports = lib.listFilePaths ./display;
   config = {
@@ -50,19 +51,13 @@ in
       ];
 
     # tty/console
-    services.kmscon =
-      let
-        fontCfg = config.home-manager.users.ash.desktop.theme.font;
-      in
-      {
-        enable = false; # FIX :: breaks display manager as of 2025-04-02
-        hwRender = false;
-        useXkbConfig = true;
-        fonts = map (font: {
-          name = font.name;
-          package = font.package;
-        }) (fontCfg.terminal ++ fontCfg.symbols);
+    services.kmscon = {
+      enable = false; # FIX :: breaks display manager as of 2025-04-02
+      useXkbConfig = true;
+      config = {
+        hwaccel = false;
       };
+    };
   };
   meta = { };
 }
